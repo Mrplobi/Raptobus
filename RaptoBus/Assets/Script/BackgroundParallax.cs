aaff1c7211236cfc41f70b0597a24b40428022e5
Lea Saunier <lea-saunier@hotmail.fr>
2020-11-20 19:07:31 +0100

    Background parallax

---
+++ RaptoBus/Assets/Script/BackgroundParallax.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundParallax : MonoBehaviour
+{
+    [SerializeField]
+    private float parallaxSpeed;
+
+    private float length;
+    private Vector3 startPos;
+
+    private float dist;
+    private Vector3 newPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Mathf.Repeat(Time.time * parallaxSpeed, length);
+        newPos = new Vector3(dist, 0, 0);
+        transform.position = startPos - newPos;
+    }
+}
